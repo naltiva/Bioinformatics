b68c9de96ac75a528f5aea6ee90766ee55a486d0
Noelia Pereira <noe.alieno@gmail.com>
2017-05-14 15:50:23 +0200

    fix count when pattern at the end of string

--- src/pattern_count.rs
+++ src/pattern_count.rs
@@ -1,10 +1,7 @@
-let text = GCGCG;
 
-let pattern = GCG;
-
-fn PatternCount(text: &str, pattern: &str) -> i32 {
+pub fn pattern_count(text: &str, pattern: &str) -> i32 {
     let mut count = 0;
-    for i in 0 ..(text.len() - pattern.len())  {
+    for i in 0 ..(text.len() - pattern.len() + 1)  {
         let slice = &text[i..(i + pattern.len())];
         if slice == pattern {
             count += 1;
