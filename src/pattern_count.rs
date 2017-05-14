ad85c79471c30fafb109af47f59d4ab4dd0a0637
Noelia Pereira <noe.alieno@gmail.com>
2017-05-14 17:25:46 +0200

    add fn frequent_words

--- src/pattern_count.rs
+++ src/pattern_count.rs
@@ -9,3 +9,26 @@ pub fn pattern_count(text: &str, pattern: &str) -> i32 {
     }
     return count;
 }
+
+
+pub fn frequent_words(text: &str, k: usize) -> Vec<&str> {
+    let mut frequent = vec![ ];
+    let mut count_pattern = vec![0 ; text.len() - k];
+    for i in 0..(text.len() - k) {
+        let read_pattern = &text[i..(i + k)];
+        let c = pattern_count(&text, &read_pattern);
+        count_pattern[i] = c;
+    }
+    let mut max = count_pattern.iter().max().unwrap();
+    for i in 0..count_pattern.len() {
+        if &count_pattern[i] == max {
+            let word = &text[i..(i + k)];
+            if !frequent.contains(&word){
+                frequent.push(&word);
+            }
+
+        }
+    }
+
+    return frequent;
+}
